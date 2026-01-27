81e7358ee9ea3c69217164eace53250ec59629a4
bernardinodamico <B.DAmico@napier.ac.uk>
2026-01-27 18:24:51 +0000

    nc

--- Scripts__/GenerateTrainingDataset.py
+++ Scripts__/GenerateTrainingDataset.py
@@ -9,7 +9,14 @@ Below is a function instantiating the DataFusion class. It builds the dataset
 for training the model parameters (and learning the graph).
 '''
 
-def gen_training_dataset():
+def gen_training_dataset(resampling: bool, sample_multiplier: int = 20):
+    
+    '''
+    param resampling: if true, the dataset is resampled using weights
+    so to be representative of the Scotland's households population
+    The final sample size of the resampled dataset is equal to the original
+    size times the multiplier
+    '''
     
     start_time = time.time()
     combined_ds_obsrv_vars = pd.DataFrame()
@@ -21,7 +28,8 @@ def gen_training_dataset():
     dp.working_status_classification()
     dp.tenure_classification()
     dp.fill_in_infrastruct_density()
-    dp.weighted_resampling(sample_size=len(dp.ds_obsrv_vars)*20)
+    if resampling is True:
+        dp.weighted_resampling(sample_size=len(dp.ds_obsrv_vars)* sample_multiplier)
     dp.rename_reorder_vars()
     
     combined_ds_obsrv_vars = dp.ds_obsrv_vars
@@ -44,4 +52,4 @@ def gen_training_dataset():
 
 
 if __name__ == "__main__":
-    gen_training_dataset()
+    gen_training_dataset(resampling=False, sample_multiplier=5)
--- notes.py
+++ notes.py
@@ -2,13 +2,21 @@ import pyAgrum as gum
 import pyAgrum.lib.image as gimg
 import pandas as pd
 import os
-os.environ["PATH"] += r";C:\Program Files\Graphviz\bin"
 
 
 
+
+
+
+
+
+
+
+
+
+  
 csv_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "DATA", "processed_dataset.csv")
 dataset = pd.read_csv(filepath_or_buffer=csv_path, sep=",")
-
 
 # 1. Initialize the learner with your dataframe
 learner = gum.BNLearner(dataset)
@@ -26,27 +34,50 @@ learner.useNMLCorrection() # Normalized Maximum Likelihood (NML) correction
 
 # Based on prior knowledge, and arch may be forbidden 
 # either because it is wrongly oriented cauaslly, or 
-# because the caputed correlation is to be expained in 
+# because the captured correlation is to be expained in 
 # terms of a confounder (latent or observed).
-learner.addForbiddenArc('Y', 'V_7') 
-learner.addForbiddenArc('Y', 'V_7') 
+for i in range(1, 15):
+    # EV ownership cannot cause any other variable in the dataset
+    learner.addForbiddenArc('Y', f'V_{i}')
+    # Root nodes cannot be caused by other variables in the dataset
+    root_nodes = ['V_3', 'V_4', 'V_9', 'V_10', 'V_13']
+    for r_node in root_nodes:
+        if i != r_node:
+            learner.addForbiddenArc(f'V_{i}', r_node)
+            #print(f'forbidden: V_{i} -> {r_node}')
 
+
+learner.addForbiddenArc('V_14', 'V_1')
+learner.addForbiddenArc('V_9', 'V_2')
+learner.addForbiddenArc('V_14', 'V_2')
+learner.addForbiddenArc('V_2', 'V_14')
+learner.addForbiddenArc('V_2', 'V_8')
+learner.addForbiddenArc('V_8', 'V_2')
+learner.addForbiddenArc('V_2', 'V_5')
+learner.addForbiddenArc('V_9', 'V_1')
 # You would force a mandatory arc because of domain 
 # knowledge (such as a known physical law or a temporal 
 # sequence) that a causal relationship exists.
-learner.addMandatoryArc('V_7', 'Y')
+learner.addMandatoryArc('V_2', 'Y')
+learner.addForbiddenArc('V_6', 'V_2')
+#learner.addMandatoryArc('V_14', 'Y')
+#learner.addMandatoryArc('V_1', 'Y')
+
+
 
 # 5. Learn the graph
-learnt_mixed_graph = learner.learnPDAG() #learnEssentialGraph() #
+learnt_mixed_graph = learner.learnPDAG()
 
 
 
 # 6. Visualize
+print(" ")
 print("--- CAUSAL DISCOVERY RESULTS ---")
 
 # 1. Get Directed Arcs (->)
 for tail, head in learnt_mixed_graph.arcs():
-    print(f"ARC: {dataset.columns[tail]} --> {dataset.columns[head]}")
+    mi = learner.correctedMutualInformation(tail, head)
+    print(f"ARC: {dataset.columns[tail]} --> {dataset.columns[head]} | MI = {round(mi, 5)}")
 
 # 2. Get Undirected Edges (-)
 for u, v in learnt_mixed_graph.edges():
